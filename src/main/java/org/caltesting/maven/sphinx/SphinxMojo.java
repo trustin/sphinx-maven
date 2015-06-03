c156861681cd34ff1f607697603bdb99932f7fc0
Bala Sridhar <balasridhar1986@gmail.com>
2015-06-03 17:20:00 -0500

    Updated the code to use annotations.

--- src/main/java/org/caltesting/maven/sphinx/SphinxMojo.java
+++ src/main/java/org/caltesting/maven/sphinx/SphinxMojo.java
@@ -1,468 +1,207 @@
 package org.caltesting.maven.sphinx;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.MavenReport;
+import org.apache.maven.reporting.MavenReportException;
+import org.codehaus.doxia.sink.Sink;
+
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
-import java.util.Set;
-import org.apache.commons.compress.archivers.ArchiveEntry;
-import org.apache.commons.compress.archivers.ArchiveInputStream;
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
-import org.apache.commons.io.IOUtils;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
-import org.apache.maven.artifact.resolver.ArtifactResolutionException;
-import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
-import org.apache.maven.doxia.siterenderer.Renderer;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.descriptor.PluginDescriptor;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.reporting.AbstractMavenReport;
-import org.apache.maven.reporting.MavenReportException;
-import org.codehaus.plexus.util.cli.CommandLineUtils;
-import org.codehaus.plexus.util.cli.Commandline;
-import org.codehaus.plexus.util.cli.StreamConsumer;
 
 /**
- * @author tomdz
- * @goal generate
- * @phase site
+ * Sphinx Mojo
+ *
+ * @author tomdz & balasridhar
  */
-public class SphinxMojo extends AbstractMavenReport
-{
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.SITE, requiresReports = true)
+public class SphinxMojo extends AbstractMojo implements MavenReport {
     /**
      * The maven project object.
-     *
-     * @parameter default-value="${project}"
-     * @required
-     * @readonly
      */
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
 
     /**
-     * The plugin's descriptor.
-     *
-     * @parameter default-value="${plugin}"
-     * @required
-     * @readonly
-     */
-    private PluginDescriptor pluginDesc;
-
-    /**
-     * The artifact factory.
-     *
-     * @component
-     */
-    private ArtifactFactory artifactFactory;
-
-    /**
-     * Needed to resolve dependencies.
-     *
-     * @component
-     */
-    private org.apache.maven.artifact.resolver.ArtifactResolver resolver;
-
-    /**
-     * Needed to resolve dependencies.
-     *
-     * @component
-     */
-    private ArtifactMetadataSource artifactMetadataSource;
-
-    /**
-     * The local repo for the project if defined;
-     *
-     * @parameter expression="${localRepository}"
-     * @required
-     * @readonly
-     */
-    private ArtifactRepository localRepository;
-
-    /**
-     * Remote repositories.
-     *
-     * @parameter expression="${project.remoteArtifactRepositories}"
-     * @required
-     * @readonly
-     */
-    private List remoteRepositories;
-
-    /**
      * The base directory of the project.
-     *
-     * @parameter default-value="${basedir}"
-     * @required
-     * @readonly
      */
+    @Parameter(defaultValue = "${basedir}", required = true, readonly = true)
     private File basedir;
 
     /**
      * The directory containing the sphinx doc source.
-     *
-     * @parameter expression="${basedir}/src/site/sphinx"
-     * @required
      */
+    @Parameter(property = "sphinx.srcDir", defaultValue = "${basedir}/src/site/sphinx", required = true)
     private File sourceDirectory;
 
     /**
      * Directory where reports will go.
-     *
-     * @parameter expression="${project.reporting.outputDirectory}"
-     * @required
      */
+    @Parameter(defaultValue = "${project.reporting.outputDirectory}", required = true, readonly = true)
     private File outputDirectory;
     
     /**
-     * Name of the report.
-     *
-     * @parameter expression="Documentation via sphinx"
-     * @required
-     */
-    private String name;
-    
-    /**
-     * Description of the report.
-     *
-     * @parameter expression="Documentation via sphinx"
-     * @required
-     */
-    private String description;
-    
-    /**
-     * The base name used to create report's output file(s).
-     *
-     * @parameter expression="index"
-     * @required
-     */
-    private String outputName;
-
-    /**
      * The directory for sphinx' source.
-     *
-     * @parameter expression="${project.build.directory}/sphinx"
-     * @required
-     * @readonly
      */
+    @Parameter(property = "sphinx.sphinxSrcDir", defaultValue = "${project.build.directory}/sphinx", required = true, readonly = true)
     private File sphinxSourceDirectory;
 
     /**
      * The builder to use. See <a href="http://sphinx.pocoo.org/man/sphinx-build.html?highlight=command%20line">sphinx-build</a>
      * for a list of supported builders.
-     *
-     * @parameter alias="builder" default-value="html"
      */
+    @Parameter(property = "sphinx.builder", required = true, alias = "builder", defaultValue = "html")
     private String builder;
 
     /**
      * The <a href="http://sphinx.pocoo.org/markup/misc.html#tags">tags</a> to pass to the sphinx build.
-     *
-     * @parameter alias="tags"
      */
+    @Parameter(property = "sphinx.tags", alias = "tags")
     private List<String> tags;
 
     /**
      * Whether Sphinx should generate verbose output.
-     *
-     * @parameter alias="verbose" default-value="true"
      */
+    @Parameter(property = "sphinx.verbose", defaultValue = "true", required = true, alias = "verbose")
     private boolean verbose;
 
     /**
      * Whether Sphinx should treat warnings as errors.
-     *
-     * @parameter alias="warningsAsErrors" default-value="false"
      */
+    @Parameter(property = "sphinx.warningAsErrors", defaultValue = "false", required = true, alias = "warningAsErrors")
     private boolean warningsAsErrors;
 
     /**
      * Whether Sphinx should generate output for all files instead of only the changed ones.
-     *
-     * @parameter alias="force" default-value="false"
      */
+    @Parameter(property = "sphinx.force", defaultValue = "false", required = true, alias = "force")
     private boolean force;
 
-    /**
-     * Whether sphinx should be run in a forked jvm instance.
-     *
-     * @parameter alias="fork" default-value="false"
-     */
-    private boolean fork;
+    /** Sphinx Executor. */
+    private final SphinxRunner sphinxRunner;
 
     /**
-     * Option to specify the jvm (or path to the java executable) to use with the forking options. For the default, the
-     * jvm will be a new instance of the same VM as the one used to run Maven. JVM settings are not inherited from
-     * MAVEN_OPTS.
-     *
-     * @parameter alias="jvm"
+     * Default Constructor.
      */
-    private String jvm;
-
-    /**
-     * Arbitrary JVM options for the forked sphinx process to set on the command line.
-     *
-     * @parameter alias="argLine"
-     */
-    private String argLine;
-
-    /**
-     * Kill the forked sphinx process after a certain number of seconds. If set to 0, wait forever for the process, never
-     * timing out.
-     *
-     * @parameter alias="forkTimeoutSec"
-     */
-    private int forkTimeoutSec;
-
-    @Override
-    public String getDescription(Locale defaultLocale)
-    {
-        return description;
+    public SphinxMojo() {
+        sphinxRunner = new SphinxRunner(getLog());
     }
 
     @Override
-    public String getName(Locale defaultLocale)
-    {
-        return name;
-    }
-
-    @Override
-    public String getOutputName()
-    {
-        return outputName;
-    }
-
-    @Override
-    public boolean isExternalReport()
-    {
-        return true;
-    }
-
-    @Override
-    protected Renderer getSiteRenderer()
-    {
-        return null;
-    }
-
-    @Override
-    protected String getOutputDirectory()
-    {
-        return outputDirectory.getAbsolutePath();
-    }
-
-    @Override
-    protected MavenProject getProject()
-    {
-        return project;
-    }
-
-    @Override
-    public void execute() throws MojoExecutionException
-    {
+    public void execute() throws MojoExecutionException {
         try {
-            executeReport(Locale.getDefault());
-        }
-        catch (MavenReportException ex) {
+            if (verbose) {
+                getLog().info("Running sphinx on " + sourceDirectory.getAbsolutePath() + ", output will be placed in "
+                        + outputDirectory.getAbsolutePath());
+            }
+
+            String[] args = getSphinxRunnerCmdLine();
+            int result;
+            try {
+                result = sphinxRunner.runSphinx(args, sphinxSourceDirectory, verbose);
+            } catch (Exception ex) {
+                throw new MavenReportException("Could not generate documentation", ex);
+            }
+            if (result != 0) {
+                throw new MavenReportException("Sphinx report generation failed");
+            }
+        } catch (MavenReportException ex) {
             throw new MojoExecutionException("Failed to run the report", ex);
         }
     }
 
     @Override
-    protected void executeReport(Locale locale) throws MavenReportException
-    {
-        unpackSphinx();
-        if (fork) {
-            runForkedSphinx();
-        }
-        else {
-            runSphinx();
-        }
-    }
-
-    private void unpackSphinx() throws MavenReportException
-    {
-        if (!sphinxSourceDirectory.exists() && !sphinxSourceDirectory.mkdirs()) {
-            throw new MavenReportException("Could not generate the temporary directory " + sphinxSourceDirectory.getAbsolutePath() + " for the sphinx sources"); 
-        }
-
-        if (verbose) {
-            getLog().info("Unpacking sphinx to " + sphinxSourceDirectory.getAbsolutePath());
-        }
+    public void generate(Sink sink, Locale locale) throws MavenReportException {
         try {
-            ArchiveInputStream input = new ArchiveStreamFactory().createArchiveInputStream("jar", SphinxMojo.class.getResourceAsStream("/sphinx.jar"));
-            ArchiveEntry entry = input.getNextEntry();
-    
-            while (entry != null) {
-                File archiveEntry = new File(sphinxSourceDirectory, entry.getName());
-                archiveEntry.getParentFile().mkdirs();
-                if (entry.isDirectory()) {
-                    archiveEntry.mkdir();
-                    entry = input.getNextEntry();
-                    continue;
-                }
-                OutputStream out = new FileOutputStream(archiveEntry);
-                IOUtils.copy(input, out);
-                out.close();
-                entry = input.getNextEntry();
-            }
-            input.close();
-        }
-        catch (Exception ex) {
-            throw new MavenReportException("Could not unpack the sphinx source", ex);
+            this.execute();
+        } catch (Exception ex) {
+            throw new MavenReportException("Error Generating Report", ex);
         }
     }
 
-    private String[] getSphinxRunnerCmdLine()
-    {
+    @Override
+    public String getOutputName() {
+        return "Sphinx";
+    }
+
+    @Override
+    public String getCategoryName() {
+        return "Sphinx";
+    }
+
+    @Override
+    public String getName(Locale locale) {
+        return "Sphinx";
+    }
+
+    @Override
+    public String getDescription(Locale locale) {
+        return "Sphinx";
+    }
+
+    @Override
+    public void setReportOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    @Override
+    public File getReportOutputDirectory() {
+        return this.outputDirectory;
+    }
+
+    @Override
+    public boolean isExternalReport() {
+        return true;
+    }
+
+    @Override
+    public boolean canGenerateReport() {
+        return true;
+    }
+
+    /**
+     * Build the Sphinx Command line options.
+     *
+     * @return
+     */
+    private String[] getSphinxRunnerCmdLine() {
         List<String> args = new ArrayList<String>();
 
-        args.add("--sphinxSourceDirectory");
-        args.add(sphinxSourceDirectory.getAbsolutePath());
         if (verbose) {
             args.add("-v");
-        }
-        else {
+        } else {
             args.add("-Q");
         }
+
         if (warningsAsErrors) {
             args.add("-W");
         }
+
         if (force) {
             args.add("-a");
             args.add("-E");
         }
-        if (builder != null) {
-            args.add("-b");
-            args.add(builder);
-        }
-        if ((tags != null) && !tags.isEmpty()) {
+
+        if (tags != null && !tags.isEmpty()) {
             for (String tag : tags) {
                 args.add("-t");
                 args.add(tag);
             }
         }
+
+        args.add("-b");
+        args.add(builder);
+
         args.add("-n");
         args.add(sourceDirectory.getAbsolutePath());
-        args.add(outputDirectory.getAbsolutePath());
+        args.add(outputDirectory.getAbsolutePath() + File.separator + builder);
         return args.toArray(new String[args.size()]);
     }
 
-    private void runSphinx() throws MavenReportException
-    {
-        if (verbose) {
-            getLog().info("Running sphinx on " + sourceDirectory.getAbsolutePath() + ", output will be placed in " + outputDirectory.getAbsolutePath());
-        }
-
-        String[] args = getSphinxRunnerCmdLine();
-
-        int result;
-        try {
-            result = SphinxRunner.run(args);
-        }
-        catch (Exception ex) {
-            throw new MavenReportException("Could not generate documentation", ex);
-        }
-        if (result != 0) {
-            throw new MavenReportException("Sphinx report generation failed");
-        }
-    }
-
-    private void runForkedSphinx() throws MavenReportException
-    {
-        String jvmBinary = jvm;
-
-        if ((jvmBinary == null) || "".equals(jvmBinary)) {
-            jvmBinary = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-        }
-        getLog().debug("Using JVM: " + jvmBinary);
-
-        Commandline cmdLine = new Commandline();
-
-        cmdLine.setWorkingDirectory(basedir);
-        cmdLine.setExecutable(jvmBinary);
-        if (argLine != null) {
-            cmdLine.createArg().setLine(argLine.replace("\n", " ").replace("\r", " "));
-        }
-        try {
-            cmdLine.addEnvironment("CLASSPATH", getPluginClasspath());
-        }
-        catch (Exception ex) {
-            throw new MavenReportException("Could not determine the classpath of the plugin", ex);
-        }
-
-        String className = SphinxRunner.class.getName();
-        String[] args = getSphinxRunnerCmdLine();
-
-        cmdLine.createArg().setValue(className);
-        for (String arg: args) {
-            cmdLine.createArg().setValue(arg);
-        }
-
-        StreamConsumer infoStreamConsumer = new StreamConsumer() {
-            @Override
-            public void consumeLine(String line)
-            {
-                getLog().info(line);
-            }
-        };
-        StreamConsumer errorStreamConsumer = new StreamConsumer() {
-            @Override
-            public void consumeLine(String line)
-            {
-                getLog().error(line);
-            }
-        };
-
-        getLog().debug(cmdLine.toString());
-
-        int result = 0;
-
-        try
-        {
-            int timeout = forkTimeoutSec > 0 ? forkTimeoutSec : 0;
-
-            result = CommandLineUtils.executeCommandLine(cmdLine,
-                                                         infoStreamConsumer,
-                                                         errorStreamConsumer,
-                                                         timeout);
-        }
-        catch (Exception ex)
-        {
-            throw new MavenReportException("Could not execute sphinx in a forked jvm", ex);
-        }
-        if (result != 0) {
-            throw new MavenReportException("Sphinx report generation failed");
-        }
-    }
-
-    private String getPluginClasspath() throws ArtifactResolutionException, ArtifactNotFoundException
-    {
-        Set<Artifact> artifacts = new HashSet<Artifact>(1);
-
-        artifacts.add(pluginDesc.getPluginArtifact());
-
-        // we need the dummy artifact here so that maven actually resolves the
-        // plugin's dependencies for us and returns them in the result object
-        Artifact originatingArtifact = artifactFactory.createBuildArtifact( "dummy", "dummy", "1.0", "jar" );
-        ArtifactResolutionResult result = resolver.resolveTransitively(artifacts,
-                                                                       originatingArtifact,
-                                                                       remoteRepositories,
-                                                                       localRepository,
-                                                                       artifactMetadataSource);
-        StringBuilder classpath = new StringBuilder();
-
-        for (Object obj : result.getArtifacts()) {
-            Artifact curArtifact = (Artifact)obj;
-
-            if (classpath.length() > 0) {
-                classpath.append(File.pathSeparatorChar);
-            }
-            classpath.append(curArtifact.getFile().getAbsolutePath());
-        }
-        return classpath.toString();
-    }
 }
--- src/main/java/org/caltesting/maven/sphinx/SphinxRunner.java
+++ src/main/java/org/caltesting/maven/sphinx/SphinxRunner.java
@@ -1,5 +1,8 @@
 package org.caltesting.maven.sphinx;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -7,55 +10,104 @@ import java.util.List;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.reporting.MavenReportException;
 import org.python.core.Py;
 import org.python.core.PySystemState;
 
-public class SphinxRunner
-{
+/**
+ * Sphinx Runner.
+ *
+ * @author Bala Sridhar & tomdz
+ * @version
+ */
+public class SphinxRunner {
+
+    /** Mojo Logger. */
+    private final Log log;
+
     /**
-     * For running sphinx via a forked jvm (or standalone).
-     *
-     * @param args
+     * Default Constructor.
+     * @param log
      */
-    public static void main(String[] args) throws ScriptException
-    {
+    public SphinxRunner(Log log) {
+        this.log = log;
+    }
+
+    /**
+     * Execute Sphinx Documentation Builder.
+     * @param args
+     * @param sphinxSourceDirectory
+     * @param verbose
+     * @return
+     * @throws Exception
+     */
+    public int runSphinx(String[] args, File sphinxSourceDirectory, boolean verbose) throws Exception {
+        unpackSphinx(sphinxSourceDirectory, verbose);
         // use headless mode for AWT (prevent "Launcher" app on Mac OS X)
         System.setProperty("java.awt.headless", "true");
 
-        System.exit(run(args));
-    }
-
-    public static int run(String[] args) throws ScriptException
-    {
         // this setting supposedly allows GCing of jython-generated classes but I'm
         // not sure if this setting has any effect on newer jython versions anymore
         System.setProperty("python.options.internalTablesImpl", "weak");
 
-        PySystemState engineSys = new PySystemState();
-        String sphinxSourceDirectory = null;
-        List<String> sphinxArgs = new ArrayList<String>(Arrays.asList(args));
-
-        for (Iterator<String> it = sphinxArgs.iterator(); it.hasNext();) {
-            String arg = it.next();
-            if ("--sphinxSourceDirectory".equals(arg) && it.hasNext()) {
-                // we need to remove it from the argument list as sphinx wouldn't like it
-                it.remove();
-                sphinxSourceDirectory = it.next();
-                it.remove();
-                break;
-            }
-        }
         if (sphinxSourceDirectory == null) {
-            throw new IllegalArgumentException("No --sphinxSourceDirectory argument given");
+            throw new IllegalArgumentException("sphinxSourceDirectory is empty.");
         }
 
-        engineSys.path.append(Py.newString(sphinxSourceDirectory));
+        PySystemState engineSys = new PySystemState();
+        engineSys.path.append(Py.newString(sphinxSourceDirectory.getAbsolutePath()));
         Py.setSystemState(engineSys);
 
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
 
-        engine.put("args", sphinxArgs.toArray(new String[sphinxArgs.size()]));
+        engine.put("args", args);
         engine.eval("import sphinx");
         return (Integer) engine.eval("sphinx.main(args)");
     }
+
+    /**
+     * Unpack Sphinx Jar file.
+     * @param sphinxSourceDirectory
+     * @param verbose
+     * @throws MavenReportException
+     */
+    private void unpackSphinx(File sphinxSourceDirectory, boolean verbose) throws MavenReportException {
+        if (!sphinxSourceDirectory.exists() && !sphinxSourceDirectory.mkdirs()) {
+            throw new MavenReportException("Could not generate the temporary directory "
+                    + sphinxSourceDirectory.getAbsolutePath() + " for the sphinx sources");
+        }
+
+        if (verbose) {
+            log.info("Unpacking sphinx to " + sphinxSourceDirectory.getAbsolutePath());
+        }
+        try {
+            ArchiveInputStream input = new ArchiveStreamFactory().createArchiveInputStream("jar",
+                    SphinxMojo.class.getResourceAsStream("/sphinx.jar"));
+            ArchiveEntry entry = input.getNextEntry();
+
+            while (entry != null) {
+                File archiveEntry = new File(sphinxSourceDirectory, entry.getName());
+                archiveEntry.getParentFile().mkdirs();
+                if (entry.isDirectory()) {
+                    archiveEntry.mkdir();
+                    entry = input.getNextEntry();
+                    continue;
+                }
+                OutputStream out = new FileOutputStream(archiveEntry);
+                IOUtils.copy(input, out);
+                out.close();
+                entry = input.getNextEntry();
+            }
+            input.close();
+        } catch (Exception ex) {
+            throw new MavenReportException("Could not unpack the sphinx source", ex);
+        }
+    }
+
 }
