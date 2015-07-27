b0b2c9e71321bdc5b424aafbb134ad78ffaaec42
Bala Sridhar <balasridhar1986@gmail.com>
2015-07-27 15:11:41 -0500

    removed unused import.

--- src/main/java/org/caltesting/maven/sphinx/SphinxMojo.java
+++ src/main/java/org/caltesting/maven/sphinx/SphinxMojo.java
@@ -5,7 +5,6 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 import org.apache.maven.reporting.MavenReport;
 import org.apache.maven.reporting.MavenReportException;
 import org.codehaus.doxia.sink.Sink;
