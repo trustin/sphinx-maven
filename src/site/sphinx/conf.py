671cc79237cbc628d62b7acfe5a263c1af0bb2f2
Trustin Lee <t@motd.kr>
2018-08-13 14:56:44 +0900

    Update site

--- src/site/sphinx/conf.py
+++ src/site/sphinx/conf.py
@@ -21,7 +21,7 @@ with open('test.yaml', 'r') as stream:
 project = u'sphinx-maven-plugin'
 copyright = u'2016, Trustin Lee et al'
 version = '2.2'
-release = '2.2.2'
+release = '2.2.3'
 
 # General options
 needs_sphinx = '1.0'
