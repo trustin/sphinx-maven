e0808dcc3f6418b769dbbafa00ab000c6bdd78a4
Trustin Lee <t@motd.kr>
2020-03-29 11:42:41 +0900

    Update sphinx-binary from 0.7.1 to 0.8.0

--- src/site/sphinx/conf.py
+++ src/site/sphinx/conf.py
@@ -20,8 +20,8 @@ with open('test.yaml', 'r') as stream:
 
 project = u'sphinx-maven-plugin'
 copyright = u'2016, Trustin Lee et al'
-version = '2.6'
-release = '2.6.0'
+version = '2.7'
+release = '2.7.0'
 
 # General options
 needs_sphinx = '1.0'
